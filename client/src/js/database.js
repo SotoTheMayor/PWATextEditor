7b8576559fbf53bc34bedbd88fbc693f2e2c6497
Soto <quickfire25@yahoo.com>
2023-04-22 13:53:33 -0500

    database file updated

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.log('PUT to the database');
+  
+  const contactDB = await openDB('contact', 1);
+  const tx = contactDB.transaction('contact', 'readwrite');
+  const store = tx.objectStore('contact');
+  const request = store.add({ content: content});
+  const result = await request;
+
+  console.log('data saved to the database', result);
+  console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.log('GET the database');
+  
+  const contactDB = await openDB('contact', 1);
+  const tx = contactDB.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+  const result = await request;
+
+  console.log('data displayed from the database', result);
+  console.error('getDb not implemented');
+}
 
 initdb();
